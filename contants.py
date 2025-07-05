eb05b6544905c6faa8733dc44587bdb027e8613b
Witold Zolnowski <blue3656@gmail.com>
2025-07-05 05:56:22 +0100

    add routing keys in constants module

---
+++ contants.py
@@ -0,0 +1,558 @@
+ROUTING_KEYS = {
+  "A92": {
+    "post_town": "Laytown-Bettystown-Mornington",
+    "post_county": "Meath"
+  },
+  "Y14": {
+    "post_town": "Arklow",
+    "post_county": "Wicklow"
+  },
+  "A84": {
+    "post_town": "Ashbourne",
+    "post_county": "Meath"
+  },
+  "H65": {
+    "post_town": "Athenry",
+    "post_county": "Galway"
+  },
+  "N37": {
+    "post_town": "Athlone",
+    "post_county": "Westmeath"
+  },
+  "R14": {
+    "post_town": "Athy",
+    "post_county": "Kildare"
+  },
+  "K32": {
+    "post_town": "Balbriggan",
+    "post_county": "Dublin"
+  },
+  "F26": {
+    "post_town": "Ballina",
+    "post_county": "Mayo"
+  },
+  "H53": {
+    "post_town": "Ballinasloe",
+    "post_county": "Galway"
+  },
+  "P31": {
+    "post_town": "Ballincollig",
+    "post_county": "Cork"
+  },
+  "F31": {
+    "post_town": "Ballinrobe",
+    "post_county": "Mayo"
+  },
+  "A75": {
+    "post_town": "Castleblaney",
+    "post_county": "Monaghan"
+  },
+  "A41": {
+    "post_town": "Ballyboughal",
+    "post_county": "Dublin"
+  },
+  "F35": {
+    "post_town": "Ballyhaunis",
+    "post_county": "Mayo"
+  },
+  "F56": {
+    "post_town": "Ballymote",
+    "post_county": "Sligo"
+  },
+  "P72": {
+    "post_town": "Bandon",
+    "post_county": "Cork"
+  },
+  "P75": {
+    "post_town": "Bantry",
+    "post_county": "Cork"
+  },
+  "H14": {
+    "post_town": "Belturbet",
+    "post_county": "Cavan"
+  },
+  "R42": {
+    "post_town": "Birr",
+    "post_county": "Offaly"
+  },
+  "A94": {
+    "post_town": "Monkstown",
+    "post_county": "Dublin"
+  },
+  "F52": {
+    "post_town": "Boyle",
+    "post_county": "Roscommon"
+  },
+  "A98": {
+    "post_town": "Bray",
+    "post_county": "Wicklow"
+  },
+  "V23": {
+    "post_town": "Caherciveen",
+    "post_county": "Kerry"
+  },
+  "E21": {
+    "post_town": "Cahir",
+    "post_county": "Tipperary"
+  },
+  "R93": {
+    "post_town": "Carlow",
+    "post_county": "Carlow"
+  },
+  "A81": {
+    "post_town": "Carrickmacross",
+    "post_county": "Monaghan"
+  },
+  "N41": {
+    "post_town": "Carrick-on-Shannon",
+    "post_county": "Leitrim"
+  },
+  "E32": {
+    "post_town": "Carrick-on-Suir",
+    "post_county": "Tipperary"
+  },
+  "P43": {
+    "post_town": "Carrigaline",
+    "post_county": "Cork"
+  },
+  "E25": {
+    "post_town": "Cashel",
+    "post_county": "Tipperary"
+  },
+  "F23": {
+    "post_town": "Castlebar",
+    "post_county": "Mayo"
+  },
+  "F45": {
+    "post_town": "Castlerea",
+    "post_county": "Roscommon"
+  },
+  "H12": {
+    "post_town": "Cavan",
+    "post_county": "Cavan"
+  },
+  "P56": {
+    "post_town": "Charleville",
+    "post_county": "Cork"
+  },
+  "F12": {
+    "post_town": "Claremorris",
+    "post_county": "Mayo"
+  },
+  "H71": {
+    "post_town": "Clifden",
+    "post_county": "Galway"
+  },
+  "P85": {
+    "post_town": "Clonakilty",
+    "post_county": "Cork"
+  },
+  "H23": {
+    "post_town": "Clones",
+    "post_county": "Monaghan"
+  },
+  "E91": {
+    "post_town": "Clonmel",
+    "post_county": "Tipperary"
+  },
+  "P24": {
+    "post_town": "Cobh",
+    "post_county": "Cork"
+  },
+  "H16": {
+    "post_town": "Cootehill",
+    "post_county": "Cavan"
+  },
+  "T12": {
+    "post_town": "Cork (centre and southside), Passage West, Cork Airport",
+    "post_county": "Cork"
+  },
+  "T23": {
+    "post_town": "Cork (northside), Blarney",
+    "post_county": "Cork"
+  },
+  "P14": {
+    "post_town": "Crookstown",
+    "post_county": "Cork"
+  },
+  "P32": {
+    "post_town": "Donoughmore",
+    "post_county": "Cork"
+  },
+  "P47": {
+    "post_town": "Dunmanway",
+    "post_county": "Cork"
+  },
+  "T56": {
+    "post_town": "Watergrasshill",
+    "post_county": "Cork"
+  },
+  "T34": {
+    "post_town": "Whitechurch",
+    "post_county": "Cork"
+  },
+  "R56": {
+    "post_town": "Curragh Camp",
+    "post_county": "Kildare"
+  },
+  "A63": {
+    "post_town": "Newtownmountkennedy",
+    "post_county": "Wicklow"
+  },
+  "F94": {
+    "post_town": "Donegal",
+    "post_county": "Donegal"
+  },
+  "D01": {
+    "post_town": "Dublin 1",
+    "post_county": "Dublin"
+  },
+  "D02": {
+    "post_town": "Dublin 2",
+    "post_county": "Dublin"
+  },
+  "D03": {
+    "post_town": "Dublin 3",
+    "post_county": "Dublin"
+  },
+  "D04": {
+    "post_town": "Dublin 4",
+    "post_county": "Dublin"
+  },
+  "D05": {
+    "post_town": "Dublin 5",
+    "post_county": "Dublin"
+  },
+  "D06": {
+    "post_town": "Dublin 6",
+    "post_county": "Dublin"
+  },
+  "D6W": {
+    "post_town": "Dublin 6W",
+    "post_county": "Dublin"
+  },
+  "D07": {
+    "post_town": "Dublin 7",
+    "post_county": "Dublin"
+  },
+  "D08": {
+    "post_town": "Dublin 8",
+    "post_county": "Dublin"
+  },
+  "D09": {
+    "post_town": "Dublin 9",
+    "post_county": "Dublin"
+  },
+  "D10": {
+    "post_town": "Dublin 10",
+    "post_county": "Dublin"
+  },
+  "D11": {
+    "post_town": "Dublin 11",
+    "post_county": "Dublin"
+  },
+  "D12": {
+    "post_town": "Dublin 12",
+    "post_county": "Dublin"
+  },
+  "D13": {
+    "post_town": "Dublin 13",
+    "post_county": "Dublin"
+  },
+  "D14": {
+    "post_town": "Dublin 14",
+    "post_county": "Dublin"
+  },
+  "D15": {
+    "post_town": "Dublin 15",
+    "post_county": "Dublin"
+  },
+  "D16": {
+    "post_town": "Dublin 16",
+    "post_county": "Dublin"
+  },
+  "D17": {
+    "post_town": "Dublin 17",
+    "post_county": "Dublin"
+  },
+  "D18": {
+    "post_town": "Dublin 18",
+    "post_county": "Dublin"
+  },
+  "D20": {
+    "post_town": "Dublin 20",
+    "post_county": "Dublin"
+  },
+  "D22": {
+    "post_town": "Dublin 22",
+    "post_county": "Dublin"
+  },
+  "D24": {
+    "post_town": "Dublin 24",
+    "post_county": "Dublin"
+  },
+  "A86": {
+    "post_town": "Dunboyne",
+    "post_county": "Meath"
+  },
+  "A91": {
+    "post_town": "Dundalk",
+    "post_county": "Louth"
+  },
+  "X35": {
+    "post_town": "Dungarvan",
+    "post_county": "Waterford"
+  },
+  "A85": {
+    "post_town": "Ratoath",
+    "post_county": "Meath"
+  },
+  "R45": {
+    "post_town": "Edenderry",
+    "post_county": "Meath"
+  },
+  "A83": {
+    "post_town": "Summerhill",
+    "post_county": "Meath"
+  },
+  "V95": {
+    "post_town": "Ennis",
+    "post_county": "Clare"
+  },
+  "Y21": {
+    "post_town": "Enniscorthy",
+    "post_county": "Wexford"
+  },
+  "P61": {
+    "post_town": "Fermoy",
+    "post_county": "Cork"
+  },
+  "H91": {
+    "post_town": "Galway",
+    "post_county": "Galway"
+  },
+  "A42": {
+    "post_town": "Garristown",
+    "post_county": "Dublin"
+  },
+  "A96": {
+    "post_town": "Glenageary",
+    "post_county": "Dublin"
+  },
+  "Y25": {
+    "post_town": "Gorey",
+    "post_county": "Wexford"
+  },
+  "A82": {
+    "post_town": "Virginia",
+    "post_county": "Cavan"
+  },
+  "R51": {
+    "post_town": "Kildare",
+    "post_county": "Kildare"
+  },
+  "R95": {
+    "post_town": "Kilkenny",
+    "post_county": "Kilkenny"
+  },
+  "V93": {
+    "post_town": "Killarney",
+    "post_county": "Kerry"
+  },
+  "X42": {
+    "post_town": "Kilmacthomas",
+    "post_county": "Waterford"
+  },
+  "V35": {
+    "post_town": "Kilmallock",
+    "post_county": "Limerick"
+  },
+  "V15": {
+    "post_town": "Kilrush",
+    "post_county": "Clare"
+  },
+  "P17": {
+    "post_town": "Kinsale",
+    "post_county": "Cork"
+  },
+  "F92": {
+    "post_town": "Letterkenny",
+    "post_county": "Donegal"
+  },
+  "F93": {
+    "post_town": "Lifford",
+    "post_county": "Donegal"
+  },
+  "V94": {
+    "post_town": "Limerick",
+    "post_county": "Limerick"
+  },
+  "V31": {
+    "post_town": "Listowel",
+    "post_county": "Kerry"
+  },
+  "T45": {
+    "post_town": "Little Island",
+    "post_county": "Cork"
+  },
+  "N39": {
+    "post_town": "Longford",
+    "post_county": "Longford"
+  },
+  "H62": {
+    "post_town": "Loughrea",
+    "post_county": "Galway"
+  },
+  "K78": {
+    "post_town": "Lucan",
+    "post_county": "Dublin"
+  },
+  "K45": {
+    "post_town": "Lusk",
+    "post_county": "Dublin"
+  },
+  "P12": {
+    "post_town": "Macroom",
+    "post_county": "Cork"
+  },
+  "K36": {
+    "post_town": "Malahide",
+    "post_county": "Dublin"
+  },
+  "P51": {
+    "post_town": "Mallow",
+    "post_county": "Cork"
+  },
+  "W23": {
+    "post_town": "Maynooth",
+    "post_county": "Kildare"
+  },
+  "P25": {
+    "post_town": "Midleton",
+    "post_county": "Cork"
+  },
+  "P67": {
+    "post_town": "Mitchelstown",
+    "post_county": "Cork"
+  },
+  "H18": {
+    "post_town": "Monaghan",
+    "post_county": "Monaghan"
+  },
+  "W34": {
+    "post_town": "Monasterevin",
+    "post_county": "Kildare"
+  },
+  "R21": {
+    "post_town": "Muine Bheag",
+    "post_county": "Carlow"
+  },
+  "N91": {
+    "post_town": "Mullingar",
+    "post_county": "Westmeath"
+  },
+  "W91": {
+    "post_town": "Naas",
+    "post_county": "Kildare"
+  },
+  "C15": {
+    "post_town": "Navan",
+    "post_county": "Meath"
+  },
+  "E45": {
+    "post_town": "Nenagh",
+    "post_county": "Tipperary"
+  },
+  "Y34": {
+    "post_town": "New Ross",
+    "post_county": "Wexford"
+  },
+  "W12": {
+    "post_town": "Newbridge",
+    "post_county": "Kildare"
+  },
+  "V42": {
+    "post_town": "Newcastle West",
+    "post_county": "Limerick"
+  },
+  "A45": {
+    "post_town": "Oldtown",
+    "post_county": "Dublin"
+  },
+  "R32": {
+    "post_town": "Portlaoise",
+    "post_county": "Laois"
+  },
+  "A67": {
+    "post_town": "Wicklow",
+    "post_county": "Wicklow"
+  },
+  "F42": {
+    "post_town": "Roscommon",
+    "post_county": "Roscommon"
+  },
+  "E53": {
+    "post_town": "Roscrea",
+    "post_county": "Tipperary"
+  },
+  "K56": {
+    "post_town": "Rush",
+    "post_county": "Dublin"
+  },
+  "V14": {
+    "post_town": "Shannon",
+    "post_county": "Clare"
+  },
+  "K34": {
+    "post_town": "Skerries",
+    "post_county": "Dublin"
+  },
+  "P81": {
+    "post_town": "Skibbereen",
+    "post_county": "Cork"
+  },
+  "F91": {
+    "post_town": "Sligo",
+    "post_county": "Sligo"
+  },
+  "K67": {
+    "post_town": "Swords and Dublin Airport",
+    "post_county": "Dublin"
+  },
+  "E41": {
+    "post_town": "Thurles",
+    "post_county": "Tipperary"
+  },
+  "E34": {
+    "post_town": "Tipperary",
+    "post_county": "Tipperary"
+  },
+  "V92": {
+    "post_town": "Tralee",
+    "post_county": "Kerry"
+  },
+  "H54": {
+    "post_town": "Tuam",
+    "post_county": "Galway"
+  },
+  "R35": {
+    "post_town": "Tullamore",
+    "post_county": "Offaly"
+  },
+  "X91": {
+    "post_town": "Waterford",
+    "post_county": "Waterford"
+  },
+  "F28": {
+    "post_town": "Westport",
+    "post_county": "Mayo"
+  },
+  "Y35": {
+    "post_town": "Wexford",
+    "post_county": "Wexford"
+  },
+  "P36": {
+    "post_town": "Youghal",
+    "post_county": "Cork"
+  }
+}
