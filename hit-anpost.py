78361f86419ce6c2717e36e57232c7537ad56a36
Witold Zolnowski <blue3656@gmail.com>
2025-07-07 05:23:02 +0100

    append results as you go

--- hit-anpost.py
+++ hit-anpost.py
@@ -2,45 +2,53 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
+# RESULTS DATA
+results_data = []
 with open('./results_A92') as f:
-    results_data = json.load(f)
-    initial_len = len(results_data)
-    next_batch = initial_len + 100
-    last_key = list(results_data)[-1]
+    for line in f:
+        line = line.strip()
+        results_data.append(json.loads(line))
+last_key = next(iter(results_data[-1]))
+
+# PERMUTATIONS
 with open('./permutations_A92') as f:
     permutations_data = json.load(f)
-
+count = 0
 catchup = True
-for EIR_CODE in permutations_data:
-    if catchup:
-        if EIR_CODE == last_key:
-            print(f"found last key = {last_key}, continuing processing keys")
-            catchup = False
-            continue
+with open('./results_A92', 'a') as fa:
+    print('last key', last_key)
+    for EIR_CODE in permutations_data:
+        if catchup:
+            if EIR_CODE == last_key:
+                print(f"found last key = {last_key}, continuing processing keys")
+                catchup = False
+                continue
+            else:
+                continue
+        print(f'processing key {EIR_CODE}')
+        url = f'https://forms.anpost.ie/enquiry/SenderDetails/SearchForAddress/?findPostalAddress={EIR_CODE}'
+        headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3', 'Content-Type': 'application/json' }
+        resp = requests.get(url, headers=headers)
+        if "No matching" in resp.text:
+            print(f"no match for {EIR_CODE}")
+            new_data = {EIR_CODE: None}
         else:
-            continue
-    print(f'processing key {EIR_CODE}')
-    url = f'https://forms.anpost.ie/enquiry/SenderDetails/SearchForAddress/?findPostalAddress={EIR_CODE}'
-    headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3', 'Content-Type': 'application/json' }
-    resp = requests.get(url, headers=headers)
-    if "No matching" in resp.text:
-        print(f"no match for {EIR_CODE}")
-        results_data[EIR_CODE] = None
-    else:
-        html = resp.text
-        soup = BeautifulSoup(html, 'lxml')
-        all_td_cells = soup.find_all('td')
-        # handle issue
-        address = all_td_cells[-1].text.strip()
-        results_data[EIR_CODE] = address
-        if len(all_td_cells) != 1:
-            print(html)
-            with open('results_A92', 'w') as f:
-                json.dump(results_data, f)
-            raise Exception('Hey, I found more/less than one table cell with address')
-        if initial_len  >= next_batch:
+            html = resp.text
+            soup = BeautifulSoup(html, 'lxml')
+            all_td_cells = soup.find_all('td')
+            # MULTIPLE RESULTS
+            if len(all_td_cells) > 1:
+                address = []
+                for a in all_td_cells:
+                    address.append(a.text.strip())
+            else:
+                address = all_td_cells[-1].text.strip()
+            new_data = {EIR_CODE: address}
+        json.dump(new_data,fa)
+        fa.write('\n')
+        count += 1
+        if count >= 1000:
             break
-with open('results_A92', 'w') as f:
-    json.dump(results_data, f)
+
 
 print('FINISHED ITERATIOS AFTER 100 records')
