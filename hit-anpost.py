263aee2e53414fa533a6f00ceccbe5124013d417
Witold Zolnowski <blue3656@gmail.com>
2025-07-08 06:11:43 +0100

    clean up?

--- hit-anpost.py
+++ hit-anpost.py
@@ -45,39 +45,43 @@ with open('LAST_PROCESSED_EIRCODE', 'r') as f:
     last_key = offset_info["last_eircode"]
     index = offset_info["eircode_index"]
 if permutations_data[index] != last_key:
-    raise Exception(f"Last processed index = {index} doesnt match the last processed EIRCODE = {last_key}")
+    raise Exception(f"Last processed index = {index} doesnt match the last processed EIRCODE = {last_key}, it equals = {permutations_data[index]}")
 
-MAX_ITEMS_TO_PROCESS = 10
+# INCREMENT ( LETS NOT PROCESS THE SAME ITEM AGAIN)
+index = index + 1
+MAX_ITEMS_TO_PROCESS = 100000
 count = 0
-with open('./results_A92', 'a') as fa:
-    for idx, EIR_CODE in enumerate(permutations_data[index:]):
-        last_key_processed = EIR_CODE
-        last_key_index = idx
-        print(f'processing key {EIR_CODE}')
-        url = f'https://forms.anpost.ie/enquiry/SenderDetails/SearchForAddress/?findPostalAddress={EIR_CODE}'
-        headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3', 'Content-Type': 'application/json' }
-        resp = requests.get(url, headers=headers)
-        if "No matching" in resp.text:
-            print(f"no match for {EIR_CODE}")
-            new_data = {EIR_CODE: None}
-        else:
-            html = resp.text
-            soup = BeautifulSoup(html, 'lxml')
-            all_td_cells = soup.find_all('td')
-            # MULTIPLE RESULTS
-            if len(all_td_cells) > 1:
-                address = []
-                for a in all_td_cells:
-                    address.append(a.text.strip())
+try:
+    with open('./results_A92', 'a') as fa:
+        for idx, EIR_CODE in enumerate(permutations_data[index]):
+            last_key_processed = EIR_CODE
+            last_key_index = idx
+            print(f'processing key {EIR_CODE}')
+            url = f'https://forms.anpost.ie/enquiry/SenderDetails/SearchForAddress/?findPostalAddress={EIR_CODE}'
+            headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3', 'Content-Type': 'application/json' }
+            resp = requests.get(url, headers=headers)
+            if "No matching" in resp.text:
+                print(f"no match for {EIR_CODE}")
+                new_data = {EIR_CODE: None}
             else:
-                address = all_td_cells[-1].text.strip()
-            new_data = {EIR_CODE: address}
-        json.dump(new_data,fa)
-        fa.write('\n')
-        count += 1
-        if count >= MAX_ITEMS_TO_PROCESS:
-            break
-
-update_last_processed_item(last_key_processed, last_key_index + index)
+                html = resp.text
+                soup = BeautifulSoup(html, 'lxml')
+                all_td_cells = soup.find_all('td')
+                # MULTIPLE RESULTS
+                if len(all_td_cells) > 1:
+                    address = []
+                    for a in all_td_cells:
+                        address.append(a.text.strip())
+                else:
+                    address = all_td_cells[-1].text.strip()
+                new_data = {EIR_CODE: address}
+            json.dump(new_data,fa)
+            fa.write('\n')
+            count += 1
+            if count >= MAX_ITEMS_TO_PROCESS:
+                break
+finally:
+    print('\nUPDATING LAST PROCESSED ITEM FILE\n')
+    update_last_processed_item(last_key_processed, last_key_index + index)
 
 print('FINISHED ITERATIOS AFTER 100 records')
